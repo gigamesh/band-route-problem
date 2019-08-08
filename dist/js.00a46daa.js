57ae244dc184b6a7f36719c275644d518268db2a
Matthew Masurka <m.masurka@gmail.com>
2019-08-07 23:17:44 -0500

    creates build in dist folder

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -21712,7 +21712,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52139" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54020" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -21888,4 +21888,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../Users/matthewmasurka/.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/js.00a46daa.js.map
+//# sourceMappingURL=js.00a46daa.js.map
